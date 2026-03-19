dafd080f2e8e2fb96e2d33f9b8a2f02a522aff25
gucio321 <gucio321@protonmail.com>
2026-03-19 10:15:39 +0100

    initial commit

---
+++ src/mytestrepo_py_gucio321/example.py
@@ -0,0 +1,9 @@
+def sum(a : int, b = 0 : int) -> int:
+    """
+    sum returns a sum of 2 integer numbers.
+
+    :param a: first number
+    :param b: second number, default is 0
+    :return: sum of a and b
+    """
+    return a + b
